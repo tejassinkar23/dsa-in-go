a9cdf71e1d0c1bc600e9ef9c40608569433d54fd
Tejas Sinkar <52974724+tejassinkar23@users.noreply.github.com>
2025-09-21 17:19:53 +0000

    Implement hasDuplicate function to check for duplicate integers in a slice

---
+++ contains_duplicate.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// func hasDuplicate(nums []int) bool {
+// 	for i := range nums {
+// 		for j := i + 1; j < len(nums); j++ {
+// 			if nums[i] == nums[j] {
+// 				return true
+// 			}
+// 		}
+// 	}
+// 	return false
+// }
+
+func hasDuplicate1(nums []int) bool {
+	seen := make(map[int]int)
+	for i, num := range nums {
+		if _, found := seen[num]; found {
+			return true
+		}
+		seen[num] = i
+	}
+	return false
+}
+
+func main() {
+	// fmt.Println("Hello World")
+	nums1 := []int{1, 2, 3, 3}
+	nums2 := []int{1, 2, 3, 4}
+	result1 := hasDuplicate1(nums1)
+	result2 := hasDuplicate1(nums2)
+	fmt.Println(result1)
+	fmt.Println(result2)
+}
